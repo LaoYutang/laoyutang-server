58491e569a9484dd1eac45f38190826a258c3850
ewen.zhang <ewen.zhang@welab-inc.com>
2023-03-31 17:36:32 +0800

    feat 初始化获取菜单权限接口

--- controllers/admin/user/get-menus.go
+++ controllers/admin/user/get-menus.go
@@ -2,15 +2,37 @@ package user
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/laoyutang/laoyutang-server/modules/db"
 	"github.com/laoyutang/laoyutang-server/modules/structs"
+	"github.com/laoyutang/laoyutang-server/utils"
+	"github.com/sirupsen/logrus"
 )
 
 func GetMenus(c *gin.Context) {
 	// 获取用户角色
 	user := &structs.User{}
-	db.Sql.Where("user-name = ?", c.MustGet("UserName")).First(user)
-	fmt.Println(user)
+	if err := db.Sql.Where("id = ?", c.MustGet("UserId")).First(user).Error; err != nil {
+		logrus.Error(err)
+		utils.ResponseFailDefault(c)
+		return
+	}
+
+	userRoles := strings.Split(user.RoleIds, ",")
+	if len(userRoles) == 0 {
+		logrus.Error(fmt.Sprintf("用户角色为空, %v ", c.MustGet("UserId")))
+		utils.ResponseSuccess(c, structs.H{})
+		return
+	}
+
+	roles := &[]structs.Role{}
+	if err := db.Sql.Where("id in ?", userRoles).First(roles).Error; err != nil {
+		logrus.Error(err)
+		utils.ResponseFailDefault(c)
+		return
+	}
+
+	logrus.Debug(utils.ToJson(roles))
 }
